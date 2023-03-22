6235803029e5333cfa29c5441700fcfefdb2cb2e
Kateryn71 <karimikatery71@gmail.com>
2023-03-22 02:36:31 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: always 0 (success)
+ * Return: always 0
  */
+
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
